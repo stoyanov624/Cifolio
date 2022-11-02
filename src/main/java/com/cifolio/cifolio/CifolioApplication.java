2d06fc774301a6e50731f4d1e5a9a2c71c1e00af
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2022-11-02 16:10:38 +0200

    Initialization of project

---
+++ src/main/java/com/cifolio/cifolio/CifolioApplication.java
@@ -0,0 +1,13 @@
+package com.cifolio.cifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CifolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cifolio/cifolio/CifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cifolio.cifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
