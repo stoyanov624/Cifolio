9abfaffd77bee0acd99bab0605a3028042b50427
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2022-11-03 12:13:24 +0200

    Implemented CityRepository for database operations, CityService for bussiness logic and a CityController for handling CRUD operations

---
+++ src/main/java/com/cifolio/cifolio/city/CityController.java
@@ -0,0 +1,19 @@
+package com.cifolio.cifolio.city;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/cities")
+public class CityController {
+    private final CityService cityService;
+
+    @Autowired
+    public CityController(CityService cityService) {
+        this.cityService = cityService;
+    }
+}
---
+++ src/main/java/com/cifolio/cifolio/city/CityService.java
@@ -0,0 +1,16 @@
+package com.cifolio.cifolio.city;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService {
+    private final CityRepository cityRepository;
+
+    @Autowired
+    public CityService(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+}
