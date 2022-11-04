56f8d837a2c21792b24aa73a639fe0c701314be3
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2022-11-04 12:57:16 +0200

    Added Cross Origins to backend API

--- src/main/java/com/cifolio/cifolio/city/CityController.java
+++ src/main/java/com/cifolio/cifolio/city/CityController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("api/cities")
 public class CityController {
     private final CityService cityService;
