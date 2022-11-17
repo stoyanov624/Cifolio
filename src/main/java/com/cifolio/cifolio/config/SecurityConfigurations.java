d8f628863a449810f1e23ed6c2cc953e1f9257bb
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2022-11-17 16:18:35 +0200

    Implemented a CustomUserDetailsService that will look into database when trying to authenticate user upon login

---
+++ src/main/java/com/cifolio/cifolio/config/SecurityConfigurations.java
@@ -0,0 +1,7 @@
+package com.cifolio.cifolio.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfigurations {
+}
--- src/main/java/com/cifolio/cifolio/controller/UserController.java
+++ src/main/java/com/cifolio/cifolio/controller/UserController.java
@@ -3,23 +3,30 @@ package com.cifolio.cifolio.controller;
 import com.cifolio.cifolio.converters.RegistrationFormToUserEntityConverter;
 import com.cifolio.cifolio.dto.RegistrationForm;
 import com.cifolio.cifolio.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class UserController {
     private final UserService userService;
     private final RegistrationFormToUserEntityConverter registrationFormToUserEntityConverter;
 
     @PostMapping("/register")
     private ResponseEntity<String> registerUser(@RequestBody RegistrationForm registrationForm) {
-        userService.registerUser(registrationFormToUserEntityConverter.apply(registrationForm));
-        return ResponseEntity.ok().body("Successful registration!");
+        try {
+            userService.registerUser(registrationFormToUserEntityConverter.apply(registrationForm));
+            return ResponseEntity.ok().body("Successful registration!");
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            return ResponseEntity.badRequest().body("Error occurred while registration attempt!");
+        }
     }
 }
