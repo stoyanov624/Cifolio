08197f476bdf919e859be52e9389f01fad0cebef
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2022-11-17 16:49:32 +0200

    Added JWT Token generation by using jwtDecoder and jwtEncoders which will work with auto generated RSA keys

--- src/main/java/com/cifolio/cifolio/config/SecurityConfigurations.java
+++ src/main/java/com/cifolio/cifolio/config/SecurityConfigurations.java
@@ -1,12 +1,27 @@
 package com.cifolio.cifolio.config;
 
 import com.cifolio.cifolio.service.user.CustomUserDetailsService;
+import com.cifolio.cifolio.utils.JWKS_Builder;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -16,6 +31,8 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @RequiredArgsConstructor
 public class SecurityConfigurations {
     private final CustomUserDetailsService customUserDetailsService;
+    private RSAKey rsaKey;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.
@@ -30,5 +47,35 @@ public class SecurityConfigurations {
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .build();
 
+    }
+
+    @Bean
+    public AuthenticationManager authManager() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(customUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        this.rsaKey = JWKS_Builder.generateRsa();
+        JWKSet jwkSet = new JWKSet(this.rsaKey);
+        return ((jwkSelector, securityContext) -> jwkSelector.select(jwkSet));
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() throws JOSEException {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwks) {
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(11);
     }
 }
---
+++ src/main/java/com/cifolio/cifolio/utils/RsaKeyGenerator.java
@@ -0,0 +1,18 @@
+package com.cifolio.cifolio.utils;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+public final class RsaKeyGenerator {
+    private RsaKeyGenerator() {}
+
+    public static KeyPair generateRsaKey() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+}
