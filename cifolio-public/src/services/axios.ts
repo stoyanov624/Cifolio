e0f3d7e7b28fb71bbdd3f35d023a2799eb763d72
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2022-11-23 23:51:44 +0200

    Added strict axios interceptor that will logout user if he is not authenticated or not authorized!

--- cifolio-public/src/services/axios.ts
+++ cifolio-public/src/services/axios.ts
@@ -1,6 +1,20 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "http://localhost:8080/api/",
-    withCredentials: true
-})
+const Axios = axios.create(
+    {
+        baseURL: "http://localhost:8080/api/",
+        withCredentials: true
+    }
+)
+// testing interceptors
+Axios.interceptors.response.use(
+    res => res,
+    error => {
+        if(error?.response?.status === 401 && window.location.pathname !== '/login' || error?.response?.status === 403) {
+            localStorage.removeItem('user');
+            window.location.replace('http://localhost:3000/login');
+        }
+        return Promise.reject(error);
+    }
+)
+export default Axios;
--- cifolio-public/src/utils/ProtectedRoutes.tsx
+++ cifolio-public/src/utils/ProtectedRoutes.tsx
@@ -1,7 +1,8 @@
 import {Navigate, Outlet} from "react-router-dom";
 
 const ProtectedRoutes = () => {
-    const isAuthenticated : boolean = Boolean(JSON.parse(localStorage.getItem('user') as string));
+    const user = localStorage.getItem('user');
+    const isAuthenticated : boolean = Boolean(user) && Boolean(JSON.parse(localStorage.getItem('user') as string));
 
     return(
         isAuthenticated ? <Outlet/> : <Navigate to={'/login'}/>
