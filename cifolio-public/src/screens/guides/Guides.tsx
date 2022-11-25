feff032703d5ede6e457de776d274e34c4df7ad6
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2022-11-25 12:06:02 +0200

    Added simple styling for travel guides screen

--- cifolio-public/src/screens/guides/Guides.tsx
+++ cifolio-public/src/screens/guides/Guides.tsx
@@ -1,5 +1,60 @@
+import './Guides.css'
+import {AiOutlineEye} from "react-icons/ai"
+
 export default function Guides() {
-    return (<>
-        </>
+    return (<div className={"guides-screen"}>
+            <h1 className={"title"}>Travel Guides</h1>
+            <div className={"guides-container"}>
+                <div>
+                    <div className={"guide-title-holder"}>
+                        <h1>Guide1</h1>
+                        <AiOutlineEye  size={25} className={"clickable"}/>
+                    </div>
+                    <h2>Guide description</h2>
+                </div>
+                <div>
+                    <div className={"guide-title-holder"}>
+                        <h1>Guide2</h1>
+                        <AiOutlineEye  size={25} className={"clickable"}/>
+                    </div>
+                    <h2>Guide description</h2>
+                </div>
+                <div>
+                    <div className={"guide-title-holder"}>
+                        <h1>Guide3</h1>
+                        <AiOutlineEye  size={25} className={"clickable"}/>
+                    </div>
+                    <h2>Guide description</h2>
+                </div>
+                <div>
+                    <div className={"guide-title-holder"}>
+                        <h1>Guide3</h1>
+                        <AiOutlineEye  size={25} className={"clickable"}/>
+                    </div>
+                    <h2>Guide description</h2>
+                </div>
+                <div>
+                    <div className={"guide-title-holder"}>
+                        <h1>Guide3</h1>
+                        <AiOutlineEye  size={25} className={"clickable"}/>
+                    </div>
+                    <h2>Guide description</h2>
+                </div>
+                <div>
+                    <div className={"guide-title-holder"}>
+                        <h1>Guide3</h1>
+                        <AiOutlineEye  size={25} className={"clickable"}/>
+                    </div>
+                    <h2>Guide description</h2>
+                </div>
+                <div>
+                    <div className={"guide-title-holder"}>
+                        <h1>Guide3</h1>
+                        <AiOutlineEye  size={25} className={"clickable"}/>
+                    </div>
+                    <h2>Guide description</h2>
+                </div>
+            </div>
+        </div>
     )
 }
